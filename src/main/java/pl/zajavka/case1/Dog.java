16a312c1679d726c2b6fd5f59f5b4a33c57fdf37
Dawid Ol <dawideq195@gmail.com>
2023-11-07 13:02:42 +0100

    Modification 2

--- src/main/java/pl/zajavka/case1/Dog.java
+++ src/main/java/pl/zajavka/case1/Dog.java
@@ -1,5 +1,10 @@
 package pl.zajavka.case1;
 
 public class Dog extends Animal {
-    private String name;
+    private String zmiany;
+
+    @Override
+    public String whatIsYourName() {
+        return "My name is Dog";
+    }
 }
